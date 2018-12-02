3c0c2cd5c45566ef76b53f6d21d8b40a4ce9523d
Sam <SamuelWare0@gmail.com>
2018-12-02 16:00:39 +0000

    changes

--- GeneticModel.py
+++ GeneticModel.py
@@ -1,42 +1,58 @@
 import random
 import numpy
 
+population = 10
 desired_value = 32
 final = None
 
-class Bits:
-	bits = ""
-
-	def __init__(self, size):
-		for i in range(size):
-			self.bits += '1' if random.random() >= 0.5 else '0'
-		
-	def get_value(self):
-		value = 0
-		for i in self.bits:
-			value += int(i)
-		return value
+def create_bits(size):
+	bits = []
+	for i in range(size):
+		bits += ["1"] if random.random() <= 0.5 else ["0"]
+	return bits
+	
+def get_value(bits):
+	value = 0
+	for i in bits:
+		value += int(i)
+	return value
 	
 total_value = 0
-starting_bits = [Bits(32) for i in range(50)]
+starting_bits = [create_bits(desired_value) for i in range(population)]
 for i in starting_bits:
-	total_value += i.get_value()
-	if i.get_value() == desired_value:
+	total_value += get_value(i)
+	if get_value(i) == desired_value:
 		final = i
-		
-print(total_value)
 
 probabilities = []
 for i in starting_bits:
-	probabilities.append(i.get_value() / total_value)
+	probabilities.append(get_value(i) / total_value)
 
 if final is not None:
-	print(final.get_value())
+	print(get_value(final))
 else:
-	for i in range(100):
-		print(numpy.random.choice(starting_bits, 1, probabilities)[0])
+	old_generation = starting_bits
 	while final is None:
-		choice1 = numpy.random.choice(starting_bits, 1, probabilities)[0]
-		choice2 = numpy.random.choice(starting_bits, 1, probabilities)[0]
+		new_generation = [["0"] * desired_value for i in range(population)]
+		for i in old_generation:
+			choice1 = numpy.random.choice(old_generation, 1, probabilities).tolist()
+			choice2 = numpy.random.choice(old_generation, 1, probabilities).tolist()
+			
+			print("1: " + str(len(choice1)))
+			print("2: " + str(len(choice1)))
+			
+			for j in choice1:
+				if random.random() <= 0.5:
+					i[random.randint(0, desired_value - 1)] = j
+				
+			for j in choice2:
+				if random.random() <= 0.5:
+					i[random.randint(0, desired_value - 1)] = j
+					
+		old_generation = new_generation
+		for i in old_generation:
+			print(len(i))
+		
+		input()
 	
 input()
